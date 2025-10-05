9adbeab67e9aa5feead51d61b9fcdf27f14ec4b0
Shiva-Singh09 <shivasingh09114@gmail.com>
2025-10-05 15:57:53 +0530

    Update index.js

--- safeHeaven/frontend/src/index.js
+++ safeHeaven/frontend/src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// ✅ Import Leaflet CSS here in JavaScript, NOT in CSS file
 import 'leaflet/dist/leaflet.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
